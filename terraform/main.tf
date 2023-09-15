b168379c34fffaebf8b35b1e2a1f47d87bc10049
Kapil Jain <kapil.jain@trilogy.com>
2023-09-15 22:33:58 +0530

    Changes done for gitignore

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,7 +28,7 @@ resource "azurerm_automation_account" "automationAccount" {
 
 # Deploy log analytics workspace
 resource "azurerm_log_analytics_workspace" "rezo" {
-  name                = "demosglaw"
+  name                = "rezoanalytics"
   location            = azurerm_resource_group.baseInfraUM_rg.location
   resource_group_name = azurerm_resource_group.baseInfraUM_rg.name
   sku                 = "PerGB2018"
